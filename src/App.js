ef486a4b4030a7bd39f94a4271d0a60a4680d681
Diogo Viana <diogocampos@outlook.com>
2020-09-06 16:40:24 -0300

    Importing search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   {
@@ -22,7 +23,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Accordion items={items} />
+        <Search />
       </div>
     );
   }
