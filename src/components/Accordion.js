98d7c6a3686c859a49a39947c09b98015b3c7241
Diogo Viana <diogocampos@outlook.com>
2020-09-06 14:57:09 -0300

    Removind doubled border and fixing style

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,19 +3,19 @@ import React from 'react';
 const Accordion = ({ items }) => {
   const renderedItems = items.map((item) => {
     return (
-      <div key={item.title}>
+      <React.Fragment key={item.title}>
         <div className="title active">
-          <i classNae="dropdown icon"></i>
+          <i className="dropdown icon"></i>
           {item.title}
         </div>
         <div className="content active">
           <p>{item.content}</p>
         </div>
-      </div>
+      </React.Fragment>
     );
   });
 
-  return <div className="ui style accordion">{renderedItems}</div>;
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
