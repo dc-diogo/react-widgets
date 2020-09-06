aafd6922b6746282bf43cec42231d625af18604f
Diogo Viana <diogocampos@outlook.com>
2020-09-06 17:18:27 -0300

    Installed axios and made request wikipedia api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
-
-  console.log('Rodo toda vez que renderiza...');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    console.log('Depois de toda renderezicao e depois da');
+    const searchWiki = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      });
+      setResults(data.query.search);
+    };
+    searchWiki();
   }, [term]);
 
   return (
