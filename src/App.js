3f7fbe3c5408cd90e78599e4f7b2d6bb12a8bce5
Diogo Viana <diogocampos@outlook.com>
2020-09-06 23:24:52 -0300

    Fix error when dropdown is toggled off and screen clicked

--- src/App.js
+++ src/App.js
@@ -36,14 +36,21 @@ const options = [
 
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div>
-      <Dropdown
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      />
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        {' '}
+        Toggle dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
